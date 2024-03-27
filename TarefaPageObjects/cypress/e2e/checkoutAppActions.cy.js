00dbf23336793d5ae49363c6ec4c89ce1ca0146b
qamichaelmaia <contatomichaelmaia@gmail.com>
2024-03-27 14:49:41 -0300

    progresso em app actions

--- TarefaPageObjects/cypress/e2e/checkoutAppActions.cy.js
+++ TarefaPageObjects/cypress/e2e/checkoutAppActions.cy.js
@@ -5,30 +5,31 @@ const { nome, telefone, end1, cidade, estado, cep } = require("../fixtures/check
 const checkoutActions = require("../support/actions/checkoutActions");
 
 describe("Teste de Checkout", () => {
-
     let email, senha;
+    let nome = "Maia";
+    let telefone = "77777777";
+    let end1 = " rua do cajueiro";
+    let cidade = "Jacobina";
+    let estado = " Bahia";
+    let cep = "77777777";
 
-    beforeEach(() => {
+    before(() => {
+        // Executa apenas uma vez antes de todos os testes
         email = loginData.email;
         senha = loginData.senha;
         cy.setCookie("ebacStoreVersion", "v2", { domain: "lojaebac.ebaconline.art.br" });
         cy.visit("/");
+        checkoutActions.login(email, senha); // Realiza o login uma vez
     });
 
     context('Adicionar Produto ao Carrinho', () => {
 
-        it("Deve fazer o login com sucesso", () => {
-            checkoutActions.login(email, senha);
-        });  
-        
-        it('Deve adicionar produto ao carrinho', () => {
+        it('Deve fazer o checkout completo', () => {
             checkoutActions.addProductToCart();
-
-        });        
-
-        it('Deve adicionar um novo endereço', () => {
             checkoutActions.addNewAddress(nome, telefone, end1, cidade, estado, cep);
-        });  
-      
+        });
+        /*it('Deve adicionar um novo endereço', () => {
+
+        });*/
     });
 });
--- TarefaPageObjects/cypress/support/actions/checkoutActions.js
+++ TarefaPageObjects/cypress/support/actions/checkoutActions.js
@@ -4,6 +4,7 @@ const checkoutActions = {
 
     login: (email, senha) => {
         cy.login(email, senha);
+        cy.visit('/')
     },
     
     addProductToCart: () => {
@@ -11,10 +12,16 @@ const checkoutActions = {
         cy.get('[style="padding: 8px;"] > :nth-child(1) > .r-18u37iz > :nth-child(1) > [data-testid="productDetails"]').click();
         cy.get('[data-testid="addToCart"]').click();
         cy.get('[data-testid="productName"]').click();
-        cy.wait(2000);
+        cy.get('[style="justify-content: space-between; flex-grow: 1;"] > :nth-child(1) > .r-1wtj0ep > .r-1i6wzkk').click()
+        cy.get('[style="background-color: rgb(242, 242, 242); display: flex;"] > :nth-child(1) > :nth-child(1) > .r-13awgt0 > .r-150rngu > :nth-child(1) > .r-14lw9ot > :nth-child(2) > .css-175oi2r > .css-146c3p1').click()
+        cy.get('.r-14lw9ot > .css-175oi2r.r-1udh08x > :nth-child(1) > .css-175oi2r').click()
+        //cy.get('[style="color: rgb(0, 0, 0); font-family: Montserrat-Medium; margin-left: 10px;"]').click()
+        //cy.get('[data-testid="selectAddressOrContinueToPayment"] > .css-146c3p1').click()
+
+        cy.wait(1000);
     },
 
-    address: (nome, telefone, end1, cidade, estado, cep) => {
+    /*address: (nome, telefone, end1, cidade, estado, cep) => {
         cy.get(':nth-child(1) > .css-175oi2r > .css-11aywtz').type(nome);
         cy.get(':nth-child(2) > .css-175oi2r > .css-11aywtz').type(telefone);
         cy.get(':nth-child(3) > .css-175oi2r > .css-11aywtz').type(end1);
@@ -22,12 +29,23 @@ const checkoutActions = {
         cy.get(':nth-child(5) > .css-175oi2r > .css-11aywtz').type(estado);
         cy.get(':nth-child(6) > .css-175oi2r > .css-11aywtz').type(cep);
         cy.wait(5000);
-    },
+    },*/
     
     addNewAddress: (nome, telefone, end1, cidade, estado, cep) => {
         // Chame a função address diretamente
-        checkoutActions.address(nome, telefone, end1, cidade, estado, cep);
-        cy.get('[data-testid="addNewAddress"] > .r-lrvibr').click();
+        //cy.get(':nth-child(1) > .css-175oi2r > .css-11aywtz').type(nome)
+        cy.get(':nth-child(2) > .css-175oi2r > .css-11aywtz').clear().type(telefone)
+        cy.get(':nth-child(3) > .css-175oi2r > .css-11aywtz').clear().type(telefone);
+        cy.get(':nth-child(4) > .css-175oi2r > .css-11aywtz').clear().type(end1);
+        cy.get(':nth-child(5) > .css-175oi2r > .css-11aywtz').clear().type(cidade);
+        cy.get(':nth-child(6) > .css-175oi2r > .css-11aywtz').clear().type(estado);
+        cy.get(':nth-child(7) > .css-175oi2r > .css-11aywtz').clear().type(cep);
+        cy.wait(5000);
+        //cy.get('[data-testid="completeCheckout"]').click()
+        //cy.get('[data-testid="goBackHome"]').should('have.text', 'Go Back Home')
+        //checkoutActions.address(nome, telefone, end1, cidade, estado, cep);
+        
+        //cy.get('[data-testid="addNewAddress"] > .r-lrvibr').click();
     }
 };
 
